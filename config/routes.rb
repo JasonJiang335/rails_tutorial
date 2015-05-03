512a353297a219acad4166e85588defc007bacd1
JasonJiang335 <jiangzidong01@gmail.com>
2015-05-03 14:04:47 -0700

    finish static page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+  # get 'static_pages/home'
+  get 'static_pages/about'
+  get 'static_pages/help'
+
   resources :weibos
   resources :users
 
@@ -8,7 +13,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   # root 'application#hello'
-  root 'users#index'
+  # root 'users#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
